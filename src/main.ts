c928e6058655e77161a8bd5dd1f822a0a995946f
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-02-18 22:42:11 +0400

    slider design end start js logic

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,25 @@ let videoCloseBtn = document.querySelector('.close-button');
 let videoPlayer = document.querySelector('video');
 let videoPopup = document.getElementById('popupV');
 
-videoBtn?.addEventListener('click', function(){
+videoBtn?.addEventListener('click', function () {
     videoPopup?.classList.add('active');
 });
-videoCloseBtn?.addEventListener('click',function(){
+videoCloseBtn?.addEventListener('click', function () {
     videoPopup?.classList.remove('active');
     videoPlayer?.pause();
 });
 // header
 
 //
-let catalogBlock = document.querySelectorAll('.catalog__block');
+let catalogBlock = document.getElementsByClassName('catalog__block');
+for (let i = 0; i < catalogBlock.length; i++) {
+    console.log(i);
+    if (i == 0 || i == 1){
+        catalogBlock[i].classList.add('visible');
+    }else{
+        catalogBlock[i].classList.add('clear');
+    }
 
+}
 
 //
